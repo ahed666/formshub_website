9c5f203b85413cdbf3ed92823c3a11388837d1e2
ahed666 <ahed0naser0eddin@gmail.com>
2024-02-26 11:17:25 +0400

    edit 2/26

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -13,7 +13,6 @@
   <meta property="og:url" content="{{ URL::current() }}">
   <meta property="og:type" content="website">
 
-  <link rel="canonical" href="{{ url()->current() }}">
 
   <!-- Favicons -->
   <link href="{{ asset('assets/img/logos/favicon.ico') }}" rel="icon">
