3b5aa6e62bc6039a799027f2666bf03ace3965fc
ahed666 <ahed0naser0eddin@gmail.com>
2024-02-28 16:32:04 +0400

    edit 2/28

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,7 +3,7 @@
   <meta name="keywords" content="Forms Hub App,UAE,Feedback web app in UAE,Feedback software,Customer satisfaction tool,Online feedback form,Feedback management system,Feedback app for businesses,
   Feedback survey tool,Best feedback tool,Feedback tool comparison,Feedback tool features,Customer feedback analytics,Real-time feedback tool,
   Feedback tool integration,Feedback tool pricing,Feedback tool for small businesses,Enterprise feedback management,Mobile feedback app,
-  Web-based feedback tool,Feedback tool for websites,Feedback tool for employee engagement, الإمارات العربية المتحدة, تطبيق ويب لمراجعة في الإمارات العربية المتحدة
+  Web-based feedback tool,Feedback tool for websites,Feedback tool for employee engagement, الإمارات العربية المتحدة, تطبيق ويب للمراجعة في الإمارات العربية المتحدة
   , برنامج مراجعة, أداة رضا العملاء, نموذج مراجعة عبر الإنترنت, نظام إدارة المراجعات, تطبيق مراجعة للشركات,
   أداة استطلاع مراجعة, أفضل أداة للمراجعة, مقارنة أدوات المراجعة, ميزات أدوات المراجعة, تحليلات مراجعة العملاء, أداة مراجعة فورية,
   تكامل أدوات المراجعة, تسعير أدوات المراجعة, أداة مراجعة للشركات الصغيرة, إدارة المراجعات للشركات الكبيرة, تطبيق مراجعة للهواتف المحمولة,
@@ -38,14 +38,14 @@
   <link href="{{ asset('assets/img/logos/favicon.ico') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href=" {{ asset('https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i') }}" rel="stylesheet">
+    <link href=" {{ asset('https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i') }}" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
