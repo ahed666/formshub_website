2522fa8967f7f71b2dd420169daddd7ea7aeda83
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-28 13:47:53 +0400

    edit 3/28

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -138,7 +138,7 @@ $text='';
                                 </div>
                                 {{-- quantity --}}
                                 <div id="quantityDiv_{{ $device->id }}" class="d-flex justify-content-center my-2 align-items-center hidden ">
-                                    <label class="fs-6"  for="quantity_with_stand_{{ $device->id }}">{{ __('Quantity: ') }}</label>
+                                    <label class="fs-6"  for="quantity_with_stand_{{ $device->id }}">{{ __('QTY: ') }}</label>
 
                                     <div class=" inline-group">
                                         <div class="input-group-prepend">
@@ -146,7 +146,7 @@ $text='';
                                             +
                                           </button>
                                         </div>
-                                        <input  class="form-control quantity w-50 mx-2 border border-1  rounded-pill px-3 " type="number" name="quantity_{{ $device->id }}" id="quantity_{{ $device->id }}" value="1" min="1" max="20" onchange="changeQuantity(this,{{ $device->id }})">
+                                        <input  class="form-control quantity w-50 mx-2 border border-1   px-3 " type="number" name="quantity_{{ $device->id }}" id="quantity_{{ $device->id }}" value="1" min="1" max="20" onchange="changeQuantity(this,{{ $device->id }})">
 
                                         <div class="input-group-append">
                                           <button class="  btn-plus">
