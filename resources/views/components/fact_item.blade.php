b5d63ed3efd2e00a459b97b377ce65a5922935de
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 15:00:16 +0400

    edit 3/30

--- resources/views/components/fact_item.blade.php
+++ resources/views/components/fact_item.blade.php
@@ -1,5 +1,7 @@
 <div class=" factitem">
-
+    <div class="fact-icon">
+       {{ $svg }}
+    </div>
     <div class="countdiv">
         <p class="number">{{ $count }}</p>
     </div>
--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -1,15 +1,20 @@
 
-
+@php
+    $responses_svg='<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+</svg>
+';
+@endphp
 <section class="fun-facts text-center">
     <div class="container">
         <div class=" facts">
 
 
-              <x-fact_item :count="$facts->signedpdf_count" :title="trans('main.signedpdf_count')"  />
-              <x-fact_item :count="$facts->responses_count" :title="trans('main.responses_count')"  />
-              <x-fact_item :count="$facts->createdforms_count" :title="trans('main.createdforms_count')"  />
-              <x-fact_item :count="$facts->linkedkiosks_count" :title="trans('main.linkedkiosks_count')"  />
-              <x-fact_item :count="$facts->createdaccount_count" :title="trans('main.createdaccount_count')"  />
+              <x-fact_item :svg="$responses_svg" :count="$facts->signedpdf_count" :title="trans('main.signedpdf_count')"  />
+              <x-fact_item :svg="$responses_svg" :count="$facts->responses_count" :title="trans('main.responses_count')"  />
+              <x-fact_item :svg="$responses_svg" :count="$facts->createdforms_count" :title="trans('main.createdforms_count')"  />
+              <x-fact_item :svg="$responses_svg" :count="$facts->linkedkiosks_count" :title="trans('main.linkedkiosks_count')"  />
+              <x-fact_item :svg="$responses_svg" :count="$facts->createdaccount_count" :title="trans('main.createdaccount_count')"  />
 
 
 
