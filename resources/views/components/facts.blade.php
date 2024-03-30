5fa40ab62f8d7ca6ff9d7a838ec6fec935c0cbe6
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 15:07:13 +0400

    edit 3/30

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -3,7 +3,8 @@ $responses_svg='<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0
     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
 </svg>
 ';
-$forms_svg='<svg class="fact_svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+$forms_svg='                    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg class="fact_svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="SVGRepo_bgCarrier" stroke-width="0" />
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
     <g id="SVGRepo_iconCarrier">
