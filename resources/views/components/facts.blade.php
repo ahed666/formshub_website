144682cdf48d5ecfd0d09969876a9178c47b2ae5
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 14:40:31 +0400

    edit 3/30

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -6,10 +6,10 @@
 
 
               <x-fact_item :count="$facts->signedpdf_count" :title="trans('main.signedpdf_count')"  />
-              <x-fact_item :count="$fact->responses_count" :title="trans('main.responses_count')"  />
-              <x-fact_item :count="$fact->createdforms_count" :title="trans('main.createdforms_count')"  />
-              <x-fact_item :count="$fact->linkedkiosks_count" :title="trans('main.linkedkiosks_count')"  />
-              <x-fact_item :count="$fact->createdaccount_count" :title="trans('main.createdaccount_count')"  />
+              <x-fact_item :count="$facts->responses_count" :title="trans('main.responses_count')"  />
+              <x-fact_item :count="$facts->createdforms_count" :title="trans('main.createdforms_count')"  />
+              <x-fact_item :count="$facts->linkedkiosks_count" :title="trans('main.linkedkiosks_count')"  />
+              <x-fact_item :count="$facts->createdaccount_count" :title="trans('main.createdaccount_count')"  />
 
 
 
