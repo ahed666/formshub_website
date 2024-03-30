34aad027ad7b641c5965b6a8f96ce2a64595d779
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 14:29:20 +0400

    edit 3/30

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -4,9 +4,7 @@
     <div class="container">
         <div class="row">
             {{ $facts }}
-             @for ($i =0 ; $i <count($facts) ; $i++)
-                   {{ $facts[$i] }}
-             @endfor
+
               {{-- <x-fact_item :count="$fact" :title="trans('main.'.$fact)"  /> --}}
 
 
