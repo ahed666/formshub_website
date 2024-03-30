2861907f52893f36cc8feec47630f91eb3d3bade
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 14:35:01 +0400

    edit 3/30

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -3,10 +3,8 @@
 <section class="fun-facts text-center">
     <div class="container">
         <div class="row">
-            {{ $facts }}
-             @foreach ($facts as $i =>$fact )
-            {{ $i  }} {{ $fact }}
-             @endforeach
+            {{ $facts->toArray()  }}
+
               {{-- <x-fact_item :count="$fact" :title="trans('main.'.$fact)"  /> --}}
 
 
