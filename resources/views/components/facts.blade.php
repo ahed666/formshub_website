f5ab59f86b48f0131c50e63a223c2d40b36f0cda
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 14:19:15 +0400

    edit 3/30

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
              @foreach ($facts as $fact )
-              <x-fact_item :count="$fact" :title="trans('main.'$fact)"  />
+              <x-fact_item :count="$fact" :title="trans('main.'.$fact)"  />
 
              @endforeach
         </div>
