147e03b4d8f5f9a46419a98d0a001176fbd081cd
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-30 14:30:48 +0400

    edit 3/30

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -4,7 +4,9 @@
     <div class="container">
         <div class="row">
             {{ $facts }}
-
+             @foreach ($facts as $i =>$fact )
+             {{ $fact}}
+             @endforeach
               {{-- <x-fact_item :count="$fact" :title="trans('main.'.$fact)"  /> --}}
 
 
