caffed222293e5f3dd7ffb2fa2b2d15f6d3553fe
ahed666 <ahed0naser0eddin@gmail.com>
2024-03-31 14:58:07 +0400

    edit 3/31

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,8 +55,8 @@
           </div>
         </div>
         <div class="row justify-content-center text-center mb-5" data-aos="fade">
-            <div class="col-md-12 mb-5 bg-custom" style="background-image: url('{{ asset('images/website_images/second_section_home.png') }}');" >
-              {{-- <img src="{{ asset('images/website_images/second_section_home.png') }}" alt="Image" class="img-fluid"> --}}
+            <div class="col-md-6 mb-5">
+              <img src="{{ asset('images/website_images/second_section_home.png') }}" alt="Image" class="img-fluid">
             </div>
           </div>
 
