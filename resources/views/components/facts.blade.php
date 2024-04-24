6428e3e9a6eded80f33b5b603227e7eefacf355e
ahed666 <ahed0naser0eddin@gmail.com>
2024-04-24 15:18:03 +0400

    edit 4/24

--- resources/views/components/facts.blade.php
+++ resources/views/components/facts.blade.php
@@ -8,7 +8,7 @@
             <x-fact_item :animate="'fade-down'"  :count="$facts->responses_count" :title="trans('main.responses_count')" />
             <x-fact_item :animate="'zoom-in'"  :count="$facts->createdforms_count" :title="trans('main.createdforms_count')" />
             <x-fact_item :animate="'fade-down'"  :count="$facts->linkedkiosks_count" :title="trans('main.linkedkiosks_count')" />
-            <x-fact_item :animate="'fade-up'"  :count="$facts->createdaccount_count" :title="trans('main.createdaccount_count')" />
+            {{-- <x-fact_item :animate="'fade-up'"  :count="$facts->createdaccount_count" :title="trans('main.createdaccount_count')" /> --}}
 
 
 
